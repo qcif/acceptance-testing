f6c5a3846ba60d5da0022ee3b401c5a546d7ebaa
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2017-07-24 00:56:25 +0000

    Deploy qcif/acceptance-testing to github.com/qcif/acceptance-testing.git:gh-pages

---
+++ cucumber/report.js
@@ -0,0 +1,64 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("login.feature");fformatter.feature({
+  "line": 2,
+  "name": "Login",
+  "description": "",
+  "id": "login",
+  "keyword": "Feature",
+  "tags": [
+    {
+      "line": 1,
+      "name": "@login"
+    }
+  ]
+formformatter.before({
+  "duration": 14174287382,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 4,
+  "name": "Select the login dialog",
+  "description": "",
+  "id": "login;select-the-login-dialog",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 5,
+  "name": "I am on the home page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "I click on login",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 7,
+  "name": "I should formatter.result({
+  "duration": 1563693331,
+  "status": "passed"
+});
+formatter.match({
+  "location": "homformatter.result({
+  "duration": 8057740883,
+  "status": "passed"
+});
+formatter.match({
+  "location": "login_steps.groovy:7"
+});
+formatter.result({
+  "duration": 460634290,
+  "status": "passed"
+});
+formatter.match({
+  "location": "login_steps.groovy:11"
+});
+formatter.result({
+  "duration": 188597928,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 149571076,
+  "status": "passed"
+});
+});
