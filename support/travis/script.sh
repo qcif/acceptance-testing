72960f0ce872a5b75a4632ee3165864ce414609d
Matthew Mulholland <matt@redboxresearchdata.com.au>
2017-07-26 22:48:36 +1000

    attempt to use build stages with bintray.

--- support/travis/script.sh
+++ support/travis/script.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-[ ! -z "${TEST_NAME}" ] || fail
 cd support/acceptance-testing && docker-compose pull && \
-docker-compose run acceptance gradle $TEST_NAME && \
+docker-compose run acceptance gradle $1 && \
 docker cp `docker ps -aqf "name=_acceptance_"`:/home/groovy/acceptance/build/reports /tmp/ && \
-tar -cvzf "${TEST_NAME}.tar.gz" /tmp/reports
+tar -cvzf "${1}.tar.gz" /tmp/reports
